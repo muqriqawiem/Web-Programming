c99c997d044779a1c19dd6985b7eb9564705f2ec
muqriqawiem <muhammadmuqri32@gmail.com>
2024-05-24 10:10:00 +0800

    Tutorial 10

---
+++ Tutorial-10/LoginPageApplication-main/README.md
@@ -0,0 +1,4 @@
+# LoginPageApplication
+
+https://github.com/Sanjanaashivanand/LoginPageApplication/assets/60909843/82e3f219-9db2-480e-a83e-9a8ab3db0526
+
---
+++ Tutorial-10/LoginPageApplication-main/backend/controllers/taskController.js
@@ -0,0 +1,12 @@
+//Create Tasks
+exports.creatTask = (req, res) => {
+    
+}
+
+
+//Get All Tasks
+exports.getAllTasks = (req, res) => {
+    res.send("All tasks")
+}
+
+    
